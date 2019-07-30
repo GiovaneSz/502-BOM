76bfc32e16b5fbfa08c0b845d5892f5d6e15d5f6
giovane <giovanesantlima@gmail.com>
2019-07-30 17:20:48 -0300

    criar adicao em local storage e deletar nome

---
+++ scripts.js
@@ -0,0 +1,19 @@
+let texto = document.querySelector("p")
+let nome = document.querySelector("input")
+let botao = document.querySelector("button")
+let Evc = document.querySelector("a")
+
+function acessar(){
+    localStorage.setItem(`nome`,nome.value);
+    texto.innerHTML =`seja bem vindo ${localStorage.nome}`;
+    Evc.innerHTML = `È voce ${localStorage.nome}`;
+}
+
+function reset(){
+    localStorage.removeItem(`nome`)
+}
+
+
+
+botao.onclick = acessar;
+Evc.onclick = reset;
